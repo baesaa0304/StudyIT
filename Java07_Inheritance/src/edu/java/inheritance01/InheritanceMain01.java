b50a598284bf21338633ca8dfe760a0e69c431e8
ITWILL <ITWILL@DESKTOP-PMIVV5U>
2023-03-17 16:06:34 +0900

    프로젝트 깃허브

---
+++ Java07_Inheritance/src/edu/java/inheritance01/InheritanceMain01.java
@@ -0,0 +1,15 @@
+package edu.java.inheritance01;
+
+
+// 상속 (inheritance) : 상위 클래스를 확장해서 하위 클래스를 설계하는 것.
+// 상위 클래스의 멤버(필드, 생성자, 메서드)들을 하위 클래스에서 사용할 수 있음.
+// 상위 (super), 부모(parent), 기본 (base) 클래스
+// 하위(sub) , 자식(child), 유도(derived) 클래스
+public class InheritanceMain01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
