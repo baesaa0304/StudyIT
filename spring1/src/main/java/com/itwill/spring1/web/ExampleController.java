9a497bddf2a751ae1f25b478d4545868b6f003b8
ITWILL <ITWILL@DESKTOP-PMIVV5U>
2023-05-17 17:52:50 +0900

    2023-05-17

--- spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
+++ spring1/src/main/java/com/itwill/spring1/web/ExampleController.java
@@ -5,6 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.itwill.spring1.dto.UserDto;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,12 +27,12 @@ public class ExampleController {
     
     @GetMapping("/") // GET 방식의 요청 주소가 "/" (context root)인 요청을 처리하는 메서드
     public String home(Model model) {
-        log.info("home");
+        log.info("home");  
         LocalDateTime now = LocalDateTime.now();
-        model.addAttribute("now", now);
+        model.addAttribute("now", now); // 뷰에 전달할 데이터를 세팅.
         
         
-        return "index"; // 뷰에 이름
+        return "index"; // 뷰에 이름(/WEB-INF/views/index.jsp)
     }
     
     @GetMapping("/ex1")
@@ -36,4 +41,55 @@ public class ExampleController {
         // 컨트롤러 메서드가 void인 경우(뷰의 이름을 리턴하지 않는 경우)
         // 요청 주소의 이름이 뷰의 이름이 됨.
     }
+    
+    @GetMapping("/ex2")
+    public void getParamEx(String username, int age) {
+        log.info("getParamEx(username={}, age={})", username, age);
+    }
+    
+    @PostMapping("/ex3")
+    public String getParamEx2(
+           @RequestParam(name = "username") String name, // (1)
+           @RequestParam(defaultValue = "0") int age // (2)
+     ) {
+        log.info("getParamEx2(name={}, age={})", name, age);
+        
+        return "ex2";
+    }
+    @GetMapping("/ex4")
+    public String getParamEx3(UserDto user) {
+        log.info("getParamEx3(user={}", user);
+        
+        return "ex2";
+    }
+    
+    @GetMapping("/sample")
+    public void sample() {
+        log.info("sample()");
+    }
+    
+    @GetMapping("/forward")
+    public String forwardTest() {
+        log.info("forwardTest()");
+        
+        // 컨트롤러 메서드에서 다른 페이지(요청 주소)로 forward 하는 방법:
+        // "forward:"으로 시작하는 문자열을 리턴하면.
+        // DispatcherServlet은 리턴값이 뷰의 이름이 아니라 포워드 이동할 페이지 주소로 인식
+        return"forward:/sample"; // 잘 사용하지 않음
+    }
+    
+    @GetMapping("/redirect")
+    public String redirectTest(RedirectAttributes attrs) {
+        log.info("redirectTest()");
+        
+        // 컨트롤러 메서드에서 리다이렉트되는 페이지까지 유지되는 정보를 저장할 때
+        attrs.addFlashAttribute("redAttr", "테스트");
+        
+        
+        // 컨트롤러 메서드에서 다른 페이지(요청 주소)로 redirect하는 방법:
+        // "redirect:"으로 시작하는 문자열을 리턴하면.
+        // DispatcherServlet은 리턴값이 뷰의 이름이 아니라 리다이렉트 이동할 페이지 주소로 인식
+        return "redirect:/sample";
+    }
+    
 }
