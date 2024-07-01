39540dd900e2abf2ba21143e5014c20e3ab81a7c
baesaa0304 <baesaa0304@naver.com>
2024-07-01 16:49:29 +0900

    커밋

--- src/main/java/hello/core/order/OrderApp.java
+++ src/main/java/hello/core/order/OrderApp.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class OrderApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 //        OrderService orderService = appConfig.orderService();
